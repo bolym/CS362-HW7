59c4399f9ca687bb6667b99750c555969c68afe5
Michael Boly <bolym@oregonstate.edu>
2021-03-03 11:09:54 -0800

    Wrote test to test for divisible by 3 printing Fizz

--- test_fizz.py
+++ test_fizz.py
@@ -4,6 +4,7 @@ import fizz_app
 class TestCase(unittest.TestCase):
     def test_add(self):
             self.assertEqual(fizz_app.fizz(1), 1)
+            self.assertEqual(fizz_app.fizz(3), "Fizz")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
