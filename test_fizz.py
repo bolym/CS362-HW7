8c64967c543b9a4e918bbe2d69ae2624ca5b7ca6
Michael Boly <bolym@oregonstate.edu>
2021-03-03 11:12:44 -0800

    Wrote test to check for divisible by 3 and 5

--- test_fizz.py
+++ test_fizz.py
@@ -5,6 +5,7 @@ class TestCase(unittest.TestCase):
     def test_add(self):
             self.assertEqual(fizz_app.fizz(1), 1)
             self.assertEqual(fizz_app.fizz(3), "Fizz")
+            self.assertEqual(fizz_app.fizz(15), "FizzBuzz")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
